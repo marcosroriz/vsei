83723752fcf2aa9ad39728c40db636d8c0fa3172
Marcos Roriz <marcosroriz@ufg.br>
2020-06-13 03:21:38 -0300

    Implementado função para buscar e listar atas

--- index.js
+++ index.js
@@ -2,36 +2,82 @@
 
 // Bibliotecas Node
 const puppeteer = require('puppeteer');
+const argv = require('minimist')(process.argv.slice(2));
+console.log(argv);
 
-// Nossas Bibliotecas
-const CREDS = require('./creds');
+// Nossos Módulos
+const CREDS = require('./creds.js');
+const NDE = require("./nde.js")
 
 // Campos do SEI
 const USERNAME_SELECTOR = "#txtUsuario";
 const PASSWORD_SELECTOR = "#pwdSenha";
 const BUTTON_SELECTOR = "#sbmLogin";
+const SEARCH_SELECTOR = "#txtPesquisaRapida";
+
+// Variavéis globais
+var browser;
+var page;
 
 
-async function run() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
-    await page.goto('https://sei.ufg.br/');
-
-    // Preenchendo dados login
+async function login(username, password) {
+    // Preenchendo dados de login
     await page.click(USERNAME_SELECTOR);
-    await page.keyboard.type(CREDS.username);
+    await page.keyboard.type(username);
     await page.waitFor(1000);
     await page.click(PASSWORD_SELECTOR);
-    await page.keyboard.type(CREDS.password);
+    await page.keyboard.type(password);
     await page.waitFor(1000);
 
-    // Login
-    
-    await page.screenshot({ path: 'screenshots/github.png' });
+    // Clicar em login
     await page.click(BUTTON_SELECTOR);
-    await page.waitForNavigation();
-    // browser.close();
+    return await page.waitForNavigation();
+}
+
+async function buscarProcesso(numProcesso) {
+    await page.waitFor(1000);
+    await page.click(SEARCH_SELECTOR);
+    await page.keyboard.type(numProcesso);
+    await page.waitFor(1000);
+    await page.keyboard.press('Enter');
+    return await page.waitForNavigation();
+}
+
+async function descobrirAtas() {
+    let iframe;
+    for (const frame of page.mainFrame().childFrames()) {
+        if (frame.name() == "ifrArvore") {
+            iframe = frame;
+            break;
+        }
+    }
+
+    return await iframe.evaluate(() => {
+        var data = {};
+        $("#frmArvore > div > div a[target='ifrVisualizacao']").each((k, v) => {
+            let treespan = $(v).find('span');
+            if (treespan.length == 1) {
+                if (treespan.attr('title').toLowerCase().includes('ata') &&
+                    !(treespan.attr('title').toLowerCase().includes('envio'))) {
+                    data[treespan.attr('title')] = v.id
+                }
+            }
+        })
+        return JSON.stringify(data);
+    });
+}
+
+async function run() {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+
+    await page.goto('https://sei.ufg.br/');
+    await login(CREDS.username, CREDS.password);
+    await buscarProcesso("23070.026856/2020-39");
+    let ataMap = await descobrirAtas();
+    console.log(ataMap);
+
+    // // browser.close();
 }
 
 run();
---
+++ nde.js
@@ -0,0 +1,12 @@
+module.exports = {
+    membros: [
+        "Elizabeth Silva De Aguiar",
+        "George Wilton Albuquerque Rangel",
+        "João Paulo Souza Silva",
+        "Marcelo Barbosa Cesar",
+        "Marcos Paulino Roriz Junior",
+        "Robinson Andrés Giraldo Zuluaga",
+        "Ronny Marcelo Aliaga Medrano",
+        "Willer Luciano Carvalho"
+    ]
+}
